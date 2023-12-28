089f7107b3922bf86fac5fc0c866bf18a7269e54
diegomoran3 <diegomoran3@gmail.com>
2023-12-28 07:17:56 +0100

    deleted not used includes

--- ElectricalGrid.h
+++ ElectricalGrid.h
@@ -1,11 +1,9 @@
 #pragma once
 #include <fstream>
 #include <iostream>
-#include <string>
 #include <vector>
 #include <map>
 #include <complex>
-#include <array>
 
 
 # define M_PI           3.14159265358979323846  /* pi */
